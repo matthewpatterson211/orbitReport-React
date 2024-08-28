10bda847dad37a3302289932406c5f95271021b2
matthewpatterson211 <56987848+matthewpatterson211@users.noreply.github.com>
2024-08-28 17:46:05 -0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,10 @@ function App() {
   const [sat, setSat] = useState(satData);
   const displaySats = [...new Set(satData.map((data) => data.orbitType))];
   const filterByType = (currentType) => {
+    
     const displaySats = satData.filter((newSatDisplay) => {
        return newSatDisplay.orbitType === currentType;
-    });
+    }) ; 
     setSat(displaySats);
  };
   return (
